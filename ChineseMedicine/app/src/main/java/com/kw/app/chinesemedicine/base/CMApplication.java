255931da68c92f4eb65e001e31f3f5cf13c0b0f3
tingyouwu <wutingyou@wxchina.com>
2016-09-13 17:36:17 +0800

    修改app包

--- ChineseMedicine/app/src/main/java/com/kw/app/chinesemedicine/base/CMApplication.java
+++ ChineseMedicine/app/src/main/java/com/kw/app/chinesemedicine/base/CMApplication.java
@@ -1,7 +1,5 @@
 package com.kw.app.chinesemedicine.base;
 
-import android.content.Context;
-
 import com.wty.app.library.base.AppConstant;
 import com.wty.app.library.base.MainApplication;
 
@@ -9,22 +7,12 @@ import cn.bmob.v3.Bmob;
 
 public class CMApplication extends MainApplication {
 
-	private static Context mApplication;
-
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		mApplication = this.getApplicationContext();
 		//初始化Bmob功能
 		Bmob.initialize(this, AppConstant.Bmob_ApplicationId);
 
-	}
-
-	/**
-	 * 功能描述：获得一个全局的application对象
-	 **/
-	public static Context getInstance(){
-		return mApplication;
 	}
 
 }
